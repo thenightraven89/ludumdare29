36b3e80e5f4e4725cf47a6b699952fc480d3a841
pulseman <bogdan.tana@gmail.com>
2014-04-27 22:54:43 +0300

    working sliding door, yay

--- src/Assets/LevelDoor.cs
+++ src/Assets/LevelDoor.cs
@@ -3,16 +3,32 @@ using System.Collections;
 
 public class LevelDoor : MonoBehaviour
 {
+    bool used = false;
 
-    // Use this for initialization
-    void Start()
+    public GameObject door1;
+    public GameObject door2;
+
+    void OnTriggerEnter(Collider other)
     {
-
+        LeanTween.moveLocalZ(door1, 1.5f, 0.2f);
+        LeanTween.moveLocalZ(door2, -1.5f, 0.2f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerExit(Collider other)
     {
+        LeanTween.moveLocalX(door1, 0f, 0.2f);
+        LeanTween.moveLocalX(door2, 0f, 0.2f);
+    }
 
+    void OnTriggerStay(Collider other)
+    {
+        if (!used)
+        {
+            if (FPSControl.instance.isInteracting)
+            {
+                used = true;
+                Debug.Log("Finished");
+            }
+        }
     }
 }
